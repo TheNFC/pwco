b96deb8652911424b0cea7caee589220710621cc
TheNFC <goncerz.michal@gmail.com>
2022-01-13 22:51:09 +0100

    Connection with DB

---
+++ pwco/Models/pwcodbContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace pwco.Models
+{
+    public partial class pwcodbContext : DbContext
+    {
+        public pwcodbContext()
+        {
+        }
+
+        public pwcodbContext(DbContextOptions<pwcodbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employees> Employees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:pwcodb.database.windows.net,1433;Initial Catalog=pwcodb;Persist Security Info=False;User ID=pwcoadmin;Password=12345abC!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employees>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.ToTable("employees");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasColumnName("email")
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .HasColumnType("numeric(18, 0)");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasColumnName("name")
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasColumnName("phone")
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Surname)
+                    .IsRequired()
+                    .HasColumnName("surname")
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
