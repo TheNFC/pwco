d9510937b69d34879e9bea1a23b4e2b6555be589
TheNFC <goncerz.michal@gmail.com>
2022-01-16 12:22:56 +0100

    Login, register and authorization.

---
+++ pwco/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using pwco.Data;
+
+[assembly: HostingStartup(typeof(pwco.Areas.Identity.IdentityHostingStartup))]
+namespace pwco.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<pwcoContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("pwcoContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<pwcoContext>();
+            });
+        }
+    }
+}
